199a3335938f6cd5d0e01c1f5ed5893b08c0fda2
Mr. C <38505354+jhwebdev@users.noreply.github.com>
2018-05-14 10:41:16 -0700

    Initial Commit

---
+++ java.js
@@ -0,0 +1,21 @@
+var galleryIndex = 1;
+showPics(galleryIndex);
+
+function plusPic(n) {
+	showPics(galleryIndex += n);
+}
+
+function currentPic(n) {
+	showPics(galleryIndex = n);
+}
+
+function showPics(n) {
+	var i;
+	var pics = document.getElementsByClassName("mySlides");
+	if(n > pics.length) {galleryIndex = 1}
+	if(n < 1) {galleryIndex = pics.length}
+	for(i = 0; i < pics.length; i++) {
+		pics[i].style.display = "none";
+	}
+	pics[galleryIndex - 1].style.display = "block";
+}
