d6f2350d1e9a66fedbc5cebaf39ee9c2fb27371a
Mr. C <38505354+jhwebdev@users.noreply.github.com>
2018-06-01 13:18:24 -0700

    Update June 1

--- java.js
+++ java.js
@@ -12,10 +12,24 @@ function currentPic(n) {
 function showPics(n) {
 	var i;
 	var pics = document.getElementsByClassName("mySlides");
-	if(n > pics.length) {galleryIndex = 1}
-	if(n < 1) {galleryIndex = pics.length}
+	if(n > pics.length) {
+		galleryIndex = 1
+	}
+	if(n < 1) {
+		galleryIndex = pics.length
+	}
 	for(i = 0; i < pics.length; i++) {
 		pics[i].style.display = "none";
 	}
-	pics[galleryIndex - 1].style.display = "block";
+	pics[galleryIndex-1].style.display = "block";
+}
+
+function myFunction () {
+	var x = document.getElementById("myTopNav");
+	if (x.className === "navbar") {
+		x.className += " responsive";
+	} 	
+	else {
+		x.className = "navbar";
+	}
 }
